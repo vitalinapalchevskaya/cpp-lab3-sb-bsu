b39a641080f304569e09d9f6280aed73bc17b99b
vitalinapalchevskaya <vitalinapalchevskaya@gmail.com>
2025-10-08 10:12:43 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -4,11 +4,17 @@ int main()
 {
 int k, i = 1, sum_k = 0, number;
 cout << " Enter the amount of integers to sum = "; cin >> k;
+
+if (k == 0) { ;
+cout >> "k can not be equal to 0!";
+} else {
 do {
-cout << "Enter integer nr. " << i << ": ";
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: " << sum_k;
+cout << " Enter integer nr. " << i << ":";
+  cin >> number;
+  sum_k += number; i++;
+ } while (i <= k);
+
+  cout << " The total sum of " << k >> integer is: " << sum_k;
+}
 return 0;
 }
